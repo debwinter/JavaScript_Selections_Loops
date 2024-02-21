23e8ba87c7bd5817f9c1dd73990516d27f70f04d
Debra Winter <deb@debwinter.com>
2024-02-21 14:30:48 -0500

    edited for console log clarity

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log("EXERCISE 1:");
 for (let i = 1; i <= 100; i++) {
     if (i % 2 == 0) {
         continue;
@@ -10,9 +10,10 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+console.log("================")
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 2:");
 for (let i = 1; i <= 100; i++) {
     if (i%3==0 && i%5==0) {
         console.log("FIZZBUZZ");
@@ -29,8 +30,10 @@ for (let i = 1; i <= 100; i++) {
         }
     }
 }
+console.log("================")
 
 // Exercise 3
+console.log("EXERCISE 3, pt 1:");
 let i = 1;
 while (i <= 100) {
     if (i%2 == 0) {
@@ -53,7 +56,9 @@ while (i <= 100) {
     }
     i++;
 }
+console.log("================")
 
+console.log("EXERCISE 3, pt 2:");
 let x = 1;
 do {
     if (x%2 == 0) {
@@ -76,8 +81,10 @@ do {
     }
     x++;
 } while (x <= 100);
+console.log("================")
 
 // Exercise 4
+console.log("EXERCISE 4:");
 let value = Math.round((Math.random() * 500));
 let n = Math.round(Math.random() * (500 - 100) + 100);
 let foundValue = false;
@@ -90,14 +97,16 @@ for (let i = 0; i <= n; i++) {
     }
 }
 if (!foundValue) console.log(`Did not find value (${value} was greater than ${n})`);
+console.log("================")
 
 // Exercise 5
+console.log("BONUS/EXERCISE 5:");
 let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
 let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
 let start = Math.round(Math.random() * (10 - 1) + 1);
 let end = Math.round(Math.random() * (1000 - 100) + 100);
 
-console.log(`COUNTING FROM ${start} TO ${end}, FIZZ @ ${fizzDivisor}, BUZZ @ ${buzzDivisor}:`)
+console.log(`counting from ${start} to ${end}, fizz @ ${fizzDivisor}, buzz @ ${buzzDivisor}:`)
 for (let i = start; i <= end; i++) {
     if (i % fizzDivisor == 0 && i % buzzDivisor == 0) {
         console.log("FIZZBUZZ");
