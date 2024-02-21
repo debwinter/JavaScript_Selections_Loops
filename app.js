299e46226a05913dacf30fa7c5df0f95efe5f7fd
Debra Winter <deb@debwinter.com>
2024-02-21 12:56:17 -0500

    corrected to be less lazy about if/else scopes

--- app.js
+++ app.js
@@ -3,67 +3,114 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 for (let i = 1; i <= 100; i++) {
-    if (i%2==0) continue;
-    else console.log(i);
+    if (i % 2 == 0) {
+        continue;
+    }
+    else {
+        console.log(i);
+    }
 }
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 for (let i = 1; i <= 100; i++) {
-    if (i%3==0 && i%5==0) console.log("FIZZBUZZ");
+    if (i%3==0 && i%5==0) {
+        console.log("FIZZBUZZ");
+    }
     else {
-        if (i%3==0) console.log("FIZZ");
-        if (i%5==0) console.log("BUZZ");
+        if (i%3==0) {
+            console.log("FIZZ");
+        }
+        else if (i%5==0) {
+            console.log("BUZZ");
+        }
+        else {
+            console.log(i);
+        }
     }
 }
 
 // Exercise 3
 let i = 1;
 while (i <= 100) {
-    if (i%2 == 0) continue;
-    if (i%3==0 && i%5==0) console.log("FIZZBUZZ");
-    else {
-        if (i%3==0) console.log("FIZZ");
-        if (i%5==0) console.log("BUZZ");
-        else console.log(i);
+    if (i%2 == 0) {
+        i++;
+        continue;
     }
+    else if (i%3==0 && i%5==0) {
+        console.log("FIZZBUZZ");
+    }
+    else {
+        if (i%3==0) {
+            console.log("FIZZ");
+        }
+        else if (i%5==0) {
+            console.log("BUZZ");
+        }
+        else {
+            console.log(i);
+        }
+    }
+    i++;
 }
 
 let x = 1;
 do {
-    if (i%2 == 0) continue;
-    if (i%3==0 && i%5==0) console.log("FIZZBUZZ");
-    else {
-        if (i%3==0) console.log("FIZZ");
-        else if (i%5==0) console.log("BUZZ");
-        else console.log(i);
+    if (x%2 == 0) {
+        x++;
+        continue;
     }
+    if (x%3==0 && x%5==0) {
+        console.log("FIZZBUZZ");
+    }
+    else {
+        if (x%3==0) {
+            console.log("FIZZ");
+        }
+        else if (x%5==0) {
+            console.log("BUZZ");
+        }
+        else {
+            console.log(x);
+        }
+    }
+    x++;
 } while (x <= 100);
 
 // Exercise 4
 let value = Math.round((Math.random() * 500));
 let n = Math.round(Math.random() * (500 - 100) + 100);
 let foundValue = false;
+
 for (let i = 0; i <= n; i++) {
     if (i === value) {
-        console.log("Found value!");
+        console.log(`Found value! (${value} was less than ${n})`);
         foundValue = true;
         break;
     }
 }
-if (!foundValue) console.log("Did not find value");
+if (!foundValue) console.log(`Did not find value (${value} was greater than ${n})`);
 
 // Exercise 5
-let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
-let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
-let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
-let end = Math.round(Math.random() * (1000 - 100) + 100); // creates a random number between 100 and 1000
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let start = Math.round(Math.random() * (10 - 1) + 1);
+let end = Math.round(Math.random() * (1000 - 100) + 100);
 
+console.log(`COUNTING FROM ${start} TO ${end}, FIZZ @ ${fizzDivisor}, BUZZ @ ${buzzDivisor}:`)
 for (let i = start; i <= end; i++) {
-    if (i % fizzDivisor == 0 && i % buzzDivisor == 0) console.log("FIZZBUZZ");
+    if (i % fizzDivisor == 0 && i % buzzDivisor == 0) {
+        console.log("FIZZBUZZ");
+    }
     else {
-        if (i % fizzDivisor == 0) console.log("FIZZ");
-        else if (i % buzzDivisor == 0) console.log("BUZZ");
-        else console.log(i);
+        if (i % fizzDivisor == 0) {
+            console.log("FIZZ");
+        }
+        else if (i % buzzDivisor == 0) {
+            console.log("BUZZ");
+        }
+        else {
+            console.log(i);
+        }
     }
 }
